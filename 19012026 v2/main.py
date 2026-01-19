6130ccd8aca81c527546f485afec4581f8cf0d09
mjennings754 <mitchellj@outlook.jp>
2026-01-20 01:03:04 +1100

    add more code

--- 19012026 v2/main.py
+++ 19012026 v2/main.py
@@ -25,3 +25,44 @@ print(last)
 # reverse the list
 reverse_list = num_list[::-1]
 print(reverse_list)
+# print the last three characters of a string
+three = "string"
+print(three[3:])
+# sort an unsorted array
+A = [2, 4, 3, 6, 7, 8, 4, 5, 9]
+
+def bubble_sort(arr):
+    n = len(arr)
+    flag = True
+    while flag:
+        flag = False
+        for i in range(1, n):
+            if arr[i-1] > arr[i]:
+                arr[i], arr[i-1] = arr[i-1], arr[i]
+                flag = True
+
+bubble_sort(A)
+print(A)
+# OR
+print(sorted(A))
+
+# define a class for a player for a video game
+class Player():
+    def __init__(self, health, dps, role):
+        self.health = health
+        self.dps = dps
+        self.role = role
+
+meiday = Player(4934, 352, "Mage")
+print(vars(meiday))
+# or you can have a __str__ method
+
+# print the highest number from a list
+num = [123, 456, 789]
+high_num = max(num)
+print(high_num)
+
+# create a for loop that iterates through how many times from input
+x = int(input("How many times to loop: "))
+for i in range(x):
+    print(i)
