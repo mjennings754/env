ebf10d2fb9756fda67d5000ef7bcfd0b01db9251
mjennings754 <mitchellj@outlook.jp>
2026-01-24 12:46:53 +1100

    add travel planner system

---
+++ 24012026/tps/main.py
@@ -0,0 +1,12 @@
+"""
+Travel Planner System - Make a system that allows users to put together their own little travel itinerary and keep track of the airline / hotel arrangements, points of interest, budget and schedule.
+"""
+destination = input("Where are you traveling to? ")
+flight_date = input("When are you flying out? ")
+airline = input("What airline are you flying with? ")
+budget = int(input("What is your budget? "))
+itinerary = []
+itinerary_input = input("What are your plans? ")
+itinerary.append(itinerary_input.split(','))
+print(f'{destination} | {flight_date} | {airline} | {budget}')
+print(itinerary)
