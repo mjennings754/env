f9f2229239460f33871f040314cd293bf43b4899
mjennings754 <mitchellj@outlook.jp>
2026-01-26 08:48:39 +1100

    add code_

--- 26012026/gym.py
+++ 26012026/gym.py
@@ -45,3 +45,69 @@ print('Min: ', data.min())
 print('Max: ', data.max())
 print('Range: ', data.range())
 print('Mean: ', data.mean())
+
+
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+"""
+Find the length of the set it_companies
+"""
+print(len(it_companies))
+"""
+Add 'Twitter' to it_companies
+"""
+it_companies.add("Twitter")
+print(it_companies)
+
+"""
+Insert multiple IT companies at once to the set it_companies
+"""
+
+it_companies.update(["Acme", "Acme2", "Acme3"])
+print(it_companies)
+
+"""
+Remove one of the companies from the set it_companies
+"""
+it_companies.pop()
+print(it_companies)
+
+"""
+Join A and B
+"""
+joinab = A.union(B)
+print(joinab)
+
+"""
+Find A intersection B
+"""
+print(A.intersection(B))
+
+"""
+Is A subset of B
+"""
+print(A.issubset(B))
+
+"""
+Are A and B disjoint sets
+"""
+print(A.isdisjoint(B))
+
+"""
+Join A with B and B with A
+"""
+join1 = A.union(B)
+join2 = B.union(A)
+
+"""
+What is the symmetric difference between A and B
+"""
+A.symmetric_difference(B)
+
+"""
+Delete the sets completely
+"""
+del A, B
