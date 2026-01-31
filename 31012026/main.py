349ce177f5cc62ed323c5f34cb0e1fc0852cbc85
mjennings754 <mitchellj@outlook.jp>
2026-01-31 17:23:30 +1100

    Add code

--- 31012026/main.py
+++ 31012026/main.py
@@ -1,3 +1,18 @@
-for i in range(10):
-    while i <= 10:
-        print(f"hello {i}")
+import json
+import csv
+
+def generate_data(name):
+    item_data = {
+        "name": name
+    }
+    return item_data
+
+data = ["Heattech Ultra Warm Crew Neck Long Sleeve", "Airism V Neck T-Shirt", "Ultra Light Down Jacket"]
+test_data = []
+for _ in range(10):
+    name = data
+
+    test_data.append(generate_data(name))
+with open("test.json", "w") as file:
+    json.dump(test_data, file, indent=1)
+print("Success")
