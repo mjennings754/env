9df6565f14cfb7ef6e04ea3d44ec6c36dc88ccc6
mjennings754 <mitchellj@outlook.jp>
2026-02-02 23:18:05 +1100

    add code

--- 02022026/main.py
+++ 02022026/main.py
@@ -8,3 +8,22 @@ class Player():
 player = Player(100)
 
 print(vars(player))
+
+empty_tuple = ()
+print(empty_tuple)
+empty_tuple = ("test", "testing", "tester")
+print(empty_tuple)
+
+number_list = []
+for number in range(1, 6):
+    number_list.append(number)
+
+print(number_list)
+
+number_thing = (number for number in range(1, 6))
+
+print(number_thing)
+
+dictionary = {'hello':'こんにちは', 'thank you':'ありがとう', 'have a nice day':'良い１日を'}
+for english, japanese in dictionary.items():
+    print(f"{english}:{japanese}")
