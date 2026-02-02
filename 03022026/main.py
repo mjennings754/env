3c5972630f693253c332707acfeedd9bd659e2eb
mjennings754 <mitchellj@outlook.jp>
2026-02-03 09:22:44 +1100

    add code

--- 03022026/main.py
+++ 03022026/main.py
@@ -56,3 +56,24 @@ print(f"${total}")
 total_two = reduce(lambda x, y: x + y, prices)
 
 print(f"${total_two}")
+
+# decorator a function that extends the behavior of another function
+
+def add_sprinkles(func):
+    def wrapper(*args, **kwargs):
+        print("*You add sprinkes")
+        func(*args, **kwargs)
+    return wrapper
+
+def add_fudge(func):
+    def wrapper(*args, **kwargs):
+        print("* You add fudge ")
+        func(*args, **kwargs)
+    return wrapper
+
+@add_sprinkles
+@add_fudge
+def get_ice_cream(flavor):
+    print(f"Here is your {flavor} ice cream [ ]")
+
+get_ice_cream("vanilla")
