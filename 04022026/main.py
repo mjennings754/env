33f06c5c1118be9b30a338fef22361a24b1f3c26
mjennings754 <mitchellj@outlook.jp>
2026-02-04 07:07:53 +1100

    Add code

---
+++ 04022026/main.py
@@ -0,0 +1,9 @@
+import random
+range_list = [1, 100]
+for i in range(random.randint(*range_list)): # * unpacks the elements of the list
+    print(i)
+
+shopping_list = []
+item = input("What item do you want to add? ")
+shopping_list.append(item)
+print(shopping_list)
