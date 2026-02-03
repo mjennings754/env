62aa4a8e845e2fb8af1aeb2a5ee47972e3a7867a
mjennings754 <mitchellj@outlook.jp>
2026-02-03 12:28:53 +1100

    add recursion

--- 03022026/main.py
+++ 03022026/main.py
@@ -116,3 +116,41 @@ print(data)
 for name, age, job in data:
     print(f"{name} is {age} years old {job}")
 
+# recursion - a function that calls itself from within
+
+#iterative
+def walk(steps):
+    for step in range(1, steps + 1):
+        print(f"You take step #{step}")
+
+walk(100)
+
+#recursive (tends to be slower but easier to right)
+def walk(steps):
+        if steps == 0:
+            return
+        walk(steps - 1)
+        print(f"You take step #{steps}")
+
+walk(100)
+
+#iterative
+
+def factorial(x):
+    result = 1
+    if x > 0:
+        for y in range(1, x + 1):
+            result *= y
+        return result
+    
+print(factorial(10))
+    
+#recursive
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return x * factorial(x - 1)
+    
+print(factorial(10))
+
