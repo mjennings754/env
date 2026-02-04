527b77413596e2d8b380c8be672568ade27ab326
mjennings754 <mitchellj@outlook.jp>
2026-02-05 10:42:01 +1100

    add code_'

--- 05022026/v2.py
+++ 05022026/v2.py
@@ -12,3 +12,24 @@ class Student:
 Harry = Student("Harry", "3.9")
 
 print(Student.get_count())
+
+import random
+
+class Player:
+    locations = []
+    possible_locations = ["Lumbridge", "Edgeville", "Varrock", "Falador"]
+
+    def __init__(self, username, world):
+        self.username = username
+        self.world = world
+        self.location = random.choice(Player.possible_locations)
+        Player.locations.append(self.location)
+        
+
+    @classmethod
+    def get_location(cls):
+        return Player.locations
+    
+Zezima = Player("Zezima", "330")
+    
+print(Player.get_location())
