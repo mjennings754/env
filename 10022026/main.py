98f4faf90c20228762a377bfc2d774045826c409
mjennings754 <mitchellj@outlook.jp>
2026-02-10 22:42:31 +1100

    add code_

---
+++ 10022026/main.py
@@ -0,0 +1,28 @@
+# Display the number of letters in the string
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+print(len(letters))
+
+# Concatenate two strings together
+string1 = "please"
+string2 = "explain"
+print(string1 + string2)
+
+# Display two strings together, with a space in between
+string1 = "please"
+string2 = "explain"
+print(string1 + " " + string2)
+
+# Use subscripting to display part of a string
+print("bazinga"[2:6])
+
+
+# Take input from the user and display that input back
+user_input = input("Type a word: ")
+print(user_input)
+
+# Display the input string converted to lower-case letters
+print(user_input.lower())
+
+# Take user input and display the number of input characters.
+print(len(user_input))
