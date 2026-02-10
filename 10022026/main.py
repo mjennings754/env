11fe41286e9aac56675858eb4de8d3b28662749c
mjennings754 <mitchellj@outlook.jp>
2026-02-10 22:48:26 +1100

    add more code

--- 10022026/main.py
+++ 10022026/main.py
@@ -26,3 +26,41 @@ print(user_input.lower())
 
 # Take user input and display the number of input characters.
 print(len(user_input))
+
+# Return the upper-case first letter entered by the user
+
+first_letter = user_input[0]
+
+print(first_letter.upper())
+
+# Store an integer as a string
+number = "6"
+
+# Convert the 'integer' string into an int object using int()
+# Multiply the integer by 7 and display the result
+
+print(int(number) * 7)
+
+# Store a floating-point number as a string
+
+float_string = "0.1"
+
+# Convert the 'float' string into a number using float()
+# Multiply the number by 7 and display the result
+
+print(float(float_string) * 7)
+
+# Create a string and an int object, then display them together
+
+stre = "mwes"
+intobj = 9
+
+print(stre + str(intobj))
+
+# Get two numbers from the user, multiply them,
+# and display the result
+num1 = int(input("Type a number: "))
+num2 = int(input("Type a number: "))
+
+result = num1 * num2
+print(result)
