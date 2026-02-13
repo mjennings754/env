face9fd53f171448e17bf71724cb63723bcc44f4
mjennings754 <mitchellj@outlook.jp>
2026-02-14 08:20:57 +1100

    add code

--- 13022026/v2.py
+++ 13022026/v2.py
@@ -8,5 +8,32 @@ print(list1)
 
 mixed = [1, 4, 5, 'Random', 6, 8, 'Random2', 4.5]
 
-numbers = [item for item in mixed if isinstance(item, (int, float))]
+numbers = [x for x in mixed if isinstance(x, (int))]
 print(numbers)
+
+nested = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print5 = nested[1][1]
+print(print5)
+
+"""
+write a function to flatten a list of lists into a single list
+"""
+list_of_lists = [[1, 2], [3, 4], [5, 6, 7]]
+def flatten(nested):
+    return [item for sublist in nested for item in sublist]
+
+res = flatten(list_of_lists)
+print(res)
+
+"""
+concatenate two lists in the following order
+
+Expected output:
+
+['Hello Dear', 'Hello Sir', 'take Dear', 'take Sir']
+
+"""
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+res = [x + y for x in list1 for y in list2]
+print(res)
