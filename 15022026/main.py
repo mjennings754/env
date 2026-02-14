e2f19bb5ce260bda24d55983584867580ed8d22f
mjennings754 <mitchellj@outlook.jp>
2026-02-15 09:25:00 +1100

    add code

--- 15022026/main.py
+++ 15022026/main.py
@@ -122,3 +122,11 @@ def addition(num):
 res = addition(10)
 print(res)
     
+"""
+Exercise 1: Generate 3 Random Integers
+
+Write a code to generate 3 random integers between 100 and 999 which is divisible by 5
+"""
+import random
+for num in range(3):
+    print(random.randrange(100, 999, 5), end=', ')
