75609569057c438ccf90e11a88f8d541d5c6eb6a
mjennings754 <mitchellj@outlook.jp>
2026-02-15 07:56:50 +1100

    add palindrome

--- 15022026/main.py
+++ 15022026/main.py
@@ -6,3 +6,16 @@ print(newlist)
 nested = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 number_5 = nested[1][1]
 print(number_5)
+
+# check if a number in palindrome
+
+def is_palindrome(n):
+    original = str(n)
+    reversed = original[::-1]
+
+    if original == reversed:
+        print("True")
+    else:
+        print("False")
+
+is_palindrome(989)
