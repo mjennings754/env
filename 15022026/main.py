a40bcf0b4d05af093ad5dfd5b5a63ed3bb998e00
mjennings754 <mitchellj@outlook.jp>
2026-02-15 07:54:11 +1100

    add code

--- 15022026/main.py
+++ 15022026/main.py
@@ -2,3 +2,7 @@ list1 = [1, 2, 3, "hello", "testing", 49.0]
 newlist = [item for item in list1 if isinstance(item, (int))]
 
 print(newlist)
+
+nested = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+number_5 = nested[1][1]
+print(number_5)
