5ef0339b155a07b8445ce19b8e2e19ed1927b101
mjennings754 <mitchellj@outlook.jp>
2026-02-15 09:26:52 +1100

    add code

--- 15022026/main.py
+++ 15022026/main.py
@@ -130,3 +130,14 @@ Write a code to generate 3 random integers between 100 and 999 which is divisibl
 import random
 for num in range(3):
     print(random.randrange(100, 999, 5), end=', ')
+
+"""
+Exercise 2: Random Lottery Pick
+
+Write a code to generate 100 random lottery tickets and pick two lucky tickets from it as a winner.
+"""
+list1 = []
+for i in range(100):
+    list1.append(random.randrange(1000000000, 9999999999))
+winners = random.sample(list1, 2)
+print(winners)
