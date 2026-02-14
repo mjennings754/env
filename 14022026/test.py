d0973d2fd42ff8ec1640a1f26ab30fa0db1b2282
mjennings754 <mitchellj@outlook.jp>
2026-02-14 22:38:37 +1100

    add code

--- 14022026/test.py
+++ 14022026/test.py
@@ -3,3 +3,8 @@ print(list1)
 newlist = [item for item in list1 if isinstance(item, (int))]
 print(newlist)
 
+list2 = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 0]
+
+while 5 in list2:
+    list2.remove(5)
+print(list2)
