71ba11c4a2d70be53d9ffe572498348a5fe7befb
mjennings754 <mitchellj@outlook.jp>
2026-02-15 12:32:49 +1100

    add exercises

--- 15022026/ex.py
+++ 15022026/ex.py
@@ -10,3 +10,30 @@ import random
 for i in range(3):
     #generate between 100 and 999
     print(random.randrange(100, 999, 5), end=", ")
+
+"""
+Exercise 2: Random Lottery Pick
+
+Write a code to generate 100 random lottery tickets and pick two lucky tickets from it as a winner.
+"""
+lottery_tickets = []
+for i in range(100):
+    lottery_tickets.append(random.randrange(1000, 9999))
+winners = random.sample(lottery_tickets, 2)
+
+print(winners)
+
+"""
+Exercise 9. Vowel Frequency Counter
+
+Practice Problem: Write a program to count the total number of vowels (a, e, i, o, u) present in a given sentence.
+"""
+
+count = 0
+vowels = 'aeiou'
+word = "Programming Python"
+for char in word:
+    if char in vowels:
+        count += 1
+
+print(count)
