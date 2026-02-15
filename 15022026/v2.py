7c1f4b4aea9bb0fb90410eb9c92ed349d757c133
mjennings754 <mitchellj@outlook.jp>
2026-02-15 22:13:24 +1100

    add code

--- 15022026/v2.py
+++ 15022026/v2.py
@@ -38,3 +38,33 @@ x = x + s1[mi:]
 
 print(x)
 
+"""
+Exercise 1: Perform Basic Tuple Operations
+
+Create a Tuple: Create a tuple named my_tuple containing the numbers 1, 2, 3, 4, and 5.
+Access Elements: Access and print the third element of my_tuple.
+Tuple Length: Find and print the length of my_tuple.
+"""
+
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple[3])
+print(len(my_tuple))
+
+"""
+Exercise 2: Tuple Repetition
+
+Repeat a below tuple three times.
+"""
+
+original_tuple = ('a', 'b')
+
+print(original_tuple * 3)
+
+"""
+Exercise 3: Slicing Tuples
+
+Slice below tuple to get elements from the 4th to the 7th position.
+"""
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+print(numbers[3:7])
