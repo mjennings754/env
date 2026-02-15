d08f3a5cc7c82e0f7dff0bb4f9d6e703b69ee97e
mjennings754 <mitchellj@outlook.jp>
2026-02-15 22:47:01 +1100

    add code

--- 15022026/v2.py
+++ 15022026/v2.py
@@ -104,3 +104,16 @@ tuple2 = (99, 88)
 tuple1, tuple2 = tuple2, tuple1
 print(tuple1)
 print(tuple2)
+
+
+"""
+Exercise 9: Copy Specific Elements From Tuple
+
+Write a program to copy elements 44 and 55 from the following tuple into a new tuple
+"""
+
+tuple1 = (11, 22, 33, 44, 55, 66)
+
+tuple2 = tuple1[3:-1]
+
+print(tuple2)
