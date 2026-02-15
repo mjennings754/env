b40552cb5b8d156d318694e8d42ed6702d47985c
mjennings754 <mitchellj@outlook.jp>
2026-02-15 22:44:08 +1100

    add code

--- 15022026/v2.py
+++ 15022026/v2.py
@@ -83,3 +83,13 @@ Write a code to access and print value 20 from given nested tuple.
 """
 tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
 print(tuple1[1][1])
+
+"""
+Exercise 7: Unpack the tuple into 4 variables
+"""
+tuple1 = (10, 20, 30, 40)
+a, b, c, d = tuple1
+print(a)
+print(b)
+print(c)
+print(d)    
