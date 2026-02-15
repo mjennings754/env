4965a0ed7b06c5c6772394739426b26e63747220
mjennings754 <mitchellj@outlook.jp>
2026-02-15 12:48:30 +1100

    add code"

--- 15022026/ex.py
+++ 15022026/ex.py
@@ -72,3 +72,14 @@ while 5 in list2:
     list2.remove(5)
 
 print(list2)
+
+"""
+Exercise 19: Iterate both lists simultaneously
+
+Given a two Python list. Write a program to iterate both lists simultaneously and display items from list1 in original order and items from list2 in reverse order.
+"""
+
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+for x, y in zip(list1, list2[::-1]):
+    print(x, y) 
