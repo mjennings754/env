99361420b693ae157bc9c256b21619750c38d069
mjennings754 <mitchellj@outlook.jp>
2026-02-17 09:36:37 +1100

    add code

--- 17022026/main.py
+++ 17022026/main.py
@@ -16,4 +16,17 @@ dict1 = {"name": "Alice", "age": 25}
 dict2 = {"city": "New York", "job": "Engineer"}
 
 dict3 = dict1 | dict2
-print(dict3)    
+print(dict3)
+
+"""
+Exercise 27. Finding Common Elements (Intersections)
+
+Practice Problem: Take two lists and find the elements that appear in both. Use Sets to perform the operation.
+"""
+list_a = [1, 2, 3, 4, 5]
+list_b = [4, 5, 6, 7, 8]
+
+set_a = set(list_a)
+set_b = set(list_b)
+common = set_a & set_b
+print(common)
