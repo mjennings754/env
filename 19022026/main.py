7590b755263707c13a76116b10708b22ba358a8a
mjennings754 <mitchellj@outlook.jp>
2026-02-19 19:51:24 +1100

    add exercise 1

--- 19022026/main.py
+++ 19022026/main.py
@@ -2,3 +2,12 @@ list1 = [1, 2, 3, "hello"]
 newlist = [item for item in list1 if isinstance(item, (int))]
 
 print(newlist)
+
+"""
+Exercise 1: List Comprehension Mastery
+
+Practice Problem: Write a single-line list comprehension that takes a list of strings, filters out strings shorter than 4 characters, and converts the remaining strings to uppercase.
+"""
+words = ["apple", "bat", "cherry", "dog", "elderberry"]
+newlist1 = [item.upper() for item in words if len(item) >= 4]
+print(newlist1)
