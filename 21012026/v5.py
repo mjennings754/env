0224972477f264fb497fd49f3030baa3f7e2739d
mjennings754 <mitchellj@outlook.jp>
2026-02-21 15:46:18 +1100

    add monkey patching

--- 21012026/v5.py
+++ 21012026/v5.py
@@ -4,3 +4,15 @@ print(my_list)
 my_dict = {i: i**2 for i in range(1, 10)}
 print(my_dict)
 
+class monkey:
+    def patch(self):
+        print("patch() is being called")
+
+def monk_p(self):
+    print("monk_p() is being called")
+
+monkey.patch = monk_p
+
+obj = monkey()
+
+obj.patch()
