6d3f0ba673772e4e1a16a9bf1d770c49f71d7177
mjennings754 <mitchellj@outlook.jp>
2026-02-23 08:35:16 +1100

    add code

--- 23022026/main.py
+++ 23022026/main.py
@@ -10,8 +10,20 @@ class Inventory():
         else:
             self.items[item] = 1
 
+    # model a purchase
+
+    def purchase(self, item):
+        if item in self.items:
+            if self.items[item] > 0:
+                self.items[item] -= 1
+            else:
+                print("error")
+
 UQJP = Inventory()
 UQJP.add_item("フリース フルジップ ジャケット")
 UQJP.add_item("フリース フルジップ ジャケット")
 UQJP.add_item("スリムフィットチノパンツ")
+UQJP.purchase("フリース フルジップ ジャケット")
+UQJP.purchase("フリース フルジップ ジャケット")
+UQJP.purchase("フリース フルジップ ジャケット")
 print(UQJP.items)
