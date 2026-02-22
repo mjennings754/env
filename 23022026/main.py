de04a83d145d04e4f356de53f2705c48317c7226
mjennings754 <mitchellj@outlook.jp>
2026-02-23 08:29:48 +1100

    add code

--- 23022026/main.py
+++ 23022026/main.py
@@ -3,11 +3,15 @@
 class Inventory():
     def __init__(self):
         self.items = {}
-
+    # what if the item already exists?
     def add_item(self, item):
-        self.items[item] = 1
-        
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
+
 UQJP = Inventory()
 UQJP.add_item("フリース フルジップ ジャケット")
 UQJP.add_item("フリース フルジップ ジャケット")
+UQJP.add_item("スリムフィットチノパンツ")
 print(UQJP.items)
