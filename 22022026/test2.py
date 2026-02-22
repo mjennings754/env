26990eac247798ac70728f9ad1e3ad3f8b93eb5d
mjennings754 <mitchellj@outlook.jp>
2026-02-22 23:35:01 +1100

    add code

--- 22022026/test2.py
+++ 22022026/test2.py
@@ -1,5 +1,7 @@
 # build an inventory system
-
+"""
+create an inventory class, set the items to dictionary to be able to track the count within the class add a function to add items to it and change the count
+"""
 class Inventory():
     def __init__(self):
         self.items = {}
@@ -10,9 +12,8 @@ class Inventory():
         else:
             self.items[item] = 1
 
-
 inv = Inventory()
 inv.add_item("apple")
-inv.add_item("apple")
+inv.add_item("eggs")
 inv.add_item("milk")
 print(inv.items)
