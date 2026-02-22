6ce407e2e92fff4de761f965234f54fd1de13a0f
mjennings754 <mitchellj@outlook.jp>
2026-02-23 09:18:45 +1100

    add code

--- 23022026/main.py
+++ 23022026/main.py
@@ -1,5 +1,5 @@
 # create an inventory system
-
+import random
 class Inventory():
     def __init__(self):
         self.items = {}
@@ -19,11 +19,25 @@ class Inventory():
             else:
                 print("error")
 
+    # how to simulate a delivery
+
+    def delivery(self):
+        itemnames = ["フリース フルジップ ジャケット", "スリムフィットチノパンツ", "フランネルシャツ", "ブロードクロスシャツ", "ワイドフィットジーンズ"]
+        quantity = random.randint(1, 200)
+        random_item = random.choice(itemnames)
+        print(f"配達\n{random_item}: {quantity}")
+        if random_item in self.items:
+            self.items[random_item] += quantity
+        else:
+            self.items[random_item] = quantity
+
+    
+
 UQJP = Inventory()
 UQJP.add_item("フリース フルジップ ジャケット")
 UQJP.add_item("フリース フルジップ ジャケット")
 UQJP.add_item("スリムフィットチノパンツ")
 UQJP.purchase("フリース フルジップ ジャケット")
 UQJP.purchase("フリース フルジップ ジャケット")
-UQJP.purchase("フリース フルジップ ジャケット")
+UQJP.delivery()
 print(UQJP.items)
