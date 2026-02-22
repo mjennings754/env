2e173d006ea73fc6ea375521837802a3336cd234
mjennings754 <mitchellj@outlook.jp>
2026-02-22 23:46:30 +1100

    add code

--- 22022026/test2.py
+++ 22022026/test2.py
@@ -2,6 +2,7 @@
 """
 create an inventory class, set the items to dictionary to be able to track the count within the class add a function to add items to it and change the count
 """
+import random
 class Inventory():
     def __init__(self):
         self.items = {}
@@ -14,15 +15,24 @@ class Inventory():
 
     def consume_item(self, item):
         if item in self.items:
-            self.items[item] -= 1
+            if self.items[item] > 0:
+                self.items[item] -= 1
+            else:
+                print(f"Cannot consume {item}, no stock left.")
         else:
-            self.items[item] = 0
+            print(f"{item} not found in inventory")
 
+    def generate_barcode(self):
+        numbers = '0123456789'
+        return ''.join(random.choices(numbers, k=24))
 
 inv = Inventory()
 inv.add_item("apple")
 inv.add_item("eggs")
 inv.add_item("eggs")
 inv.consume_item("eggs") # should remove the count
+inv.consume_item("eggs")
+inv.consume_item("eggs")
 inv.add_item("milk")
+print(inv.generate_barcode())
 print(inv.items)
