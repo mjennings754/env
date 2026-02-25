75ab5def33a8b6b6b21940e3162318f526b28c7f
mjennings754 <mitchellj@outlook.jp>
2026-02-26 07:53:41 +1100

    add linked list

--- 26022026/main.py
+++ 26022026/main.py
@@ -10,3 +10,27 @@ class Inventory():
 Amazon = Inventory()
 Amazon.add_book("Programming Python")
 print(Amazon.books)
+
+# create a linked list
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def traverse(head):
+    currentNode = head
+    while currentNode:
+        print(currentNode.data, end=" -> ")
+        currentNode = currentNode.next
+    print("null")
+
+node1 = Node(8)
+node2 = Node(12)
+node3 = Node(2)
+node4 = Node(5)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+traverse(node1)
