d3b7ed093d5e4489dc0e9ab494d92ea0e8306541
mjennings754 <mitchellj@outlook.jp>
2026-02-26 11:50:06 +1100

    add code

---
+++ 26022026/gym.py
@@ -0,0 +1,10 @@
+"""
+Exercise 1: Create a function in Python
+
+Write a program to create a function that takes two arguments, name and age, and prints their values.
+"""
+
+def take_input(name, age):
+    print(name, age)
+
+take_input("Mitchell", "99")
