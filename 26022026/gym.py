711b4f69df979772dc0a13dfa084994fdf21a508
mjennings754 <mitchellj@outlook.jp>
2026-02-26 11:51:48 +1100

    add code

--- 26022026/gym.py
+++ 26022026/gym.py
@@ -8,3 +8,14 @@ def take_input(name, age):
     print(name, age)
 
 take_input("Mitchell", "99")
+
+
+"""
+Exercise 2: Create a function with variable length of arguments
+"""
+
+def func1(*args):
+    for arg in args:
+        print(arg)
+
+func1(20, 40, 60)
