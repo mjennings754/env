aa3331be814e5654b5b05945190a557f9cd8be05
mjennings754 <mitchellj@outlook.jp>
2026-02-26 12:00:24 +1100

    add code

--- 26022026/gym.py
+++ 26022026/gym.py
@@ -30,3 +30,18 @@ def calculation(a, b):
 
 res = calculation(40, 10)
 print(res)
+
+"""
+Exercise 4: Create a function with a default argument
+
+Write a program to create a function show_employee() with the following specifications:
+
+It should accept the employee’s name and salary.
+It should display both the name and salary.
+If the salary is not provided in the function call, it should default to 9000.
+"""
+def show_employee(name, salary=9000):
+    print(f"{name} salary: {salary}")
+
+show_employee("Ben", 12000)
+show_employee("Ben")
