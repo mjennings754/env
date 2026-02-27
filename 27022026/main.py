1eb3df303a7bfd6171bd5c4ff48363107d07306c
mjennings754 <mitchellj@outlook.jp>
2026-02-27 15:56:35 +1100

    add code

--- 27022026/main.py
+++ 27022026/main.py
@@ -21,3 +21,62 @@ def alphabet_sum(words):
 
 words = ["apple", "dog", "library"]
 print(alphabet_sum(words))
+
+# Implement a priority queue using a linked list.
+class Node:
+    def __init__(self, data, priority):
+        self.data = data
+        self.priority = priority
+        self.next = None
+
+class PriorityQueue:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+    
+    def enqueue(self, data, priority):
+        new_node = Node(data, priority)
+
+        if self.is_empty() or priority > self.head.priority:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next and current.next.priority >= priority:
+                current = current.next
+            new_node.next = current.next
+            current.next = new_node
+
+    def dequeue(self):
+        if self.is_empty():
+            raise Exception("Priority queue is empty")
+        
+        removed_data = self.head.data
+        self.head = self.head.next
+        return removed_data
+    
+    def peek(self):
+        if self.is_empty():
+            raise Exception("Priority Queue is empty")
+        
+        return self.head.data
+    
+    def display(self):
+        current = self.head
+        while current:
+            print(f"Data: {current.data}, Priority: {current.priority}")
+            current = current.next
+
+queue = PriorityQueue()
+
+queue.enqueue("Task A", 2)
+queue.enqueue("Task B", 1)
+queue.enqueue("Task C", 3)
+queue.enqueue("Task D", 4)
+queue.enqueue("Task E", 5)
+
+queue.display()
+
+print("Dequeued:", queue.dequeue())
