52ad791275a86bf848f6b6d94b88d045f870fcd2
mjennings754 <mitchellj@outlook.jp>
2026-03-01 07:44:50 +1100

    add code

--- 01032026/main.py
+++ 01032026/main.py
@@ -10,3 +10,23 @@ words = ["apple", "bat", "cherry", "dog", "elderberry"]
 
 list1 = [word.upper() for word in words if len(word) >= 4]
 print(list1)
+
+"""
+Exercise 2: Dictionary Merging with Logic
+
+Practice Problem: Write a function that merges two dictionaries. If a key exists in both dictionaries, sum their values. If a key exists in only one, include it as is.
+Given Input: dict_a = {'a': 10, 'b': 20} dict_b = {'b': 5, 'c': 15}
+
+Expected Output: Merged Dictionary: {'a': 10, 'b': 25, 'c': 15}
+"""
+def merge(dict_a, dict_b):
+    res = dict_a.copy()
+
+    for key, value in dict_b.items():
+        res[key] = res.get(key, 0) + value
+    return res
+dict_a = {'a': 10, 'b': 20} 
+dict_b = {'b': 5, 'c': 15}
+
+merged = merge(dict_a, dict_b)
+print(merged)
