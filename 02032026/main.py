3de6052ca34e88d43c83a2f378f0b98b78cbc2e1
mjennings754 <mitchellj@outlook.jp>
2026-03-02 09:17:34 +1100

    add code

---
+++ 02032026/main.py
@@ -0,0 +1,18 @@
+# sort the list
+list1 = [2, 6, 543, 23, 5, 8, 7, 5, 534, 867, 876, 675]
+
+#list1.sort()
+#print(list1)
+
+def sort(x):
+    n = len(x)
+    flag = True
+    while flag:
+        flag = False
+        for i in range(1, n):
+            if x[i-1] > x[i]:
+                flag = True
+                x[i-1], x[i] = x[i], x[i-1]
+
+sort(list1)
+print(list1)
