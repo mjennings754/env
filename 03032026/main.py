90cd5bfcff7f2dd8681be2e6c8d612dbe16b55aa
mjennings754 <mitchellj@outlook.jp>
2026-03-03 09:14:15 +1100

    add code

--- 03032026/main.py
+++ 03032026/main.py
@@ -3,3 +3,12 @@ import datetime
 now = datetime.datetime.now()
 
 print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+# calcuate the area of a circle based on the radius input
+from math import pi
+
+r = float(input("Enter the radius: "))
+
+area = pi * r ** 2
+
+print(area)
