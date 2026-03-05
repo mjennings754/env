5f7295809dc3a3a212b761fda52bfbab05039e0b
mjennings754 <mitchellj@outlook.jp>
2026-03-05 13:51:28 +1100

    add code

---
+++ 05032026/main.py
@@ -0,0 +1,14 @@
+"""Exercise 1A: Create a string made of the first, middle and last character"""
+str1 = "James"
+
+res = str1[0]
+
+l = len(str1)
+
+mi = int(l / 2)
+
+res = res + str1[mi]
+
+res = res + str1[l - 1]
+
+print(res)
