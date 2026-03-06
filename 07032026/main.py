09b246799e5b24eafd64f428ac9e518cdb3958e1
mjennings754 <mitchellj@outlook.jp>
2026-03-07 08:49:25 +1100

    add lamda dict sorting

--- 07032026/main.py
+++ 07032026/main.py
@@ -84,3 +84,12 @@ authors = {
     "Huxley": ["Brave New World"]
 }
 print(invert_dict(authors))
+
+"""
+Exercise 13: Dictionary Sorting (Lambda)
+"""
+employees = [{"name": "A", "salary": 50}, {"name": "B", "salary": 70}, {"name": "C", "salary": 60}]
+
+sorted_employees = sorted(employees, key=lambda x:x['salary'], reverse=True)
+for emp in sorted_employees:
+    print(emp)
