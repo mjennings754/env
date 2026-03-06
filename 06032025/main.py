5e08037ff61378f144e9179c44b8907fb41c8c10
mjennings754 <mitchellj@outlook.jp>
2026-03-06 16:15:28 +1100

    add code

--- 06032025/main.py
+++ 06032025/main.py
@@ -6,3 +6,23 @@ Practice Problem: Write a single-line list comprehension that takes a list of st
 words = ["apple", "bat", "cherry", "dog", "elderberry"]
 newlist1 = [word.upper() for word in words if len(word) >= 4]
 print(newlist1)
+
+"""
+Exercise 2: Dictionary Merging with Logic
+
+Practice Problem: Write a function that merges two dictionaries. If a key exists in both dictionaries, sum their values. If a key exists in only one, include it as is.
+"""
+dict_a = {'a': 10, 'b': 20}
+dict_b = {'b': 5, 'c': 15}
+
+def merge_dict(dict_a, dict_b):
+    res = dict_a.copy()
+
+    for key, value in dict_b.items():
+        res[key] = res.get(key, 0) + value
+
+    return res
+
+
+merged = merge_dict(dict_a, dict_b)
+print(merged)
