01d23e5849bc066fe7262d2f122e84d3300fcdbc
mjennings754 <mitchellj@outlook.jp>
2026-03-08 08:55:51 +1100

    return multiple values from a function

--- 08032026/main.py
+++ 08032026/main.py
@@ -20,3 +20,16 @@ def func1(*args):
         print(arg)
 
 func1(20, 30, 40)
+
+"""
+Exercise 3: Return multiple values from a function
+
+Write a function calculation() that accepts two variables and calculates both their addition and subtraction. The function should then return both the sum and the difference in a single return statement.
+"""
+def calculation(a, b):
+    addition = a + b
+    subtraction = a - b
+    return(addition, subtraction)
+
+res = calculation(40, 10)
+print(res)
