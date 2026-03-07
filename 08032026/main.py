d7cdd5b593c43ae10496c8917816e7f37b04814a
mjennings754 <mitchellj@outlook.jp>
2026-03-08 08:54:26 +1100

    add variable length of arguments

--- 08032026/main.py
+++ 08032026/main.py
@@ -8,3 +8,15 @@ def display(name, age):
     print(name, age)
 
 display("test", 99)
+
+"""
+Exercise 2: Create a function with variable length of arguments
+
+Write a program to create a function func1() that accepts a variable number of arguments and prints each of their values.
+"""
+
+def func1(*args):
+    for arg in args:
+        print(arg)
+
+func1(20, 30, 40)
