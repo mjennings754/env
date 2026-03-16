b374a5b7c85f0da85d387e1b7b2c0bf80b46e81d
mjennings754 <mitchellj@outlook.jp>
2026-03-16 13:44:16 +1100

    add code

--- 16032026/main.py
+++ 16032026/main.py
@@ -4,3 +4,10 @@ Exercise 16: Use a lambda with the map() function to double each element in a li
 numbers = [1, 2, 3, 4, 5]
 doubled = list(map(lambda x: x * 2, numbers))
 print(doubled)
+
+"""
+Exercise 17: Use a lambda with the sorted() function to sort a list of tuples based on the second element
+"""
+data = [('apple', 5), ('banana', 2), ('cherry', 8), ('date', 1)]
+sorted = sorted(data, key=lambda item: item[1])
+print(sorted)
