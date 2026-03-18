e636990a6309017e88509de708df72c00f050c4e
mjennings754 <mitchellj@outlook.jp>
2026-03-18 23:14:29 +1100

    add code

--- 18032026/main.py
+++ 18032026/main.py
@@ -3,3 +3,21 @@
 words = ["apple", "bat", "cherry", "dog", "elderberry"]
 newlist = [word.upper() for word in words if len(word) >= 4]
 print(newlist)
+
+# flatten a nested list
+
+
+def flatten(lst):
+    flat_list = []
+
+    for item in lst:
+        if isinstance(item, list):
+            flat_list.extend(flatten(item))
+        else:
+            flat_list.append(item)
+
+    return flat_list
+
+nested = [1, [2, 3], [4, [5, 6]], 7]
+res = flatten(nested)
+print(res)
