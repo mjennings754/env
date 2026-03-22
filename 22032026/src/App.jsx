ee1c6a1115294e164a3b2075b95166a8e863d9e7
mjennings754 <mitchellj@outlook.jp>
2026-03-22 22:42:23 +1100

    add drop party

---
+++ 22032026/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button"
+
+function App() {
+
+
+  return (
+    <>
+    <Button />
+    </>
+  )
+}
+
+export default App
---
+++ 22032026/src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button() {
+const dropparty = () => {
+    console.log("Drop party starting in 10...")
+}
+    return (
+        <button onClick={dropparty}>Click me!</button>
+    )
+}
+export default Button;
