eeb2cf75168c2560425a9d3e78b6ee97b5b24bfb
mjennings754 <mitchellj@outlook.jp>
2026-03-30 09:41:23 +1100

    add api response

--- 30032026/main.py
+++ 30032026/main.py
@@ -1,3 +1,9 @@
 squares = "#"
 squares = [x for x in squares * 5]
 print(squares)
+
+import requests
+
+response = requests.get("https://api.github.com")
+print(response.status_code)
+print(response.json())
