e0421a06124ff4a1ec3315e77cfbc509a9a8afd0
mjennings754 <mitchellj@outlook.jp>
2026-03-31 09:13:29 +1100

    add code

---
+++ 31032026/main.py
@@ -0,0 +1,4 @@
+quests = ["Dragon Slayer 2", "Monkey Madness 2", "Song of the Elves", "Desert Treasure 2", "The Waterfall Quest"]
+
+testing = [quest for quest in quests if quest[0] == "D"]
+print(testing)
